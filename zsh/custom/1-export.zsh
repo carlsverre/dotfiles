57a672772bc4819cc209a1067fe608cd920758ca
Carl Sverre <carl@f0a.org>
2022-04-05 01:07:42 +0000

    antigen

--- zsh/custom/1-export.zsh
+++ zsh/custom/1-export.zsh
@@ -1,3 +1,2 @@
 export PATH="${HOME}/localbin:${HOME}/bin:/usr/local/bin:${PATH}"
-
 export LANG=en_US.UTF-8
--- zsh/install.sh
+++ zsh/install.sh
@@ -2,15 +2,8 @@
 
 set -eEuo pipefail
 
-ZSH="${HOME}/.oh-my-zsh"
-ZSH_CUSTOM="${HOME}/.config/zsh-custom"
-
-echo "Installing oh-my-zsh"
-if [ -d "${ZSH}" ]; then
-    echo "oh-my-zsh is already installed"
-else
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+ZSH_HOME="${HOME}/.config/zsh"
 
 ln -sfn ${DOTFILES_LOCATION}/zsh/zshrc ${HOME}/.zshrc
-ln -sfn ${DOTFILES_LOCATION}/zsh/custom ${ZSH_CUSTOM}
+ln -sfn ${DOTFILES_LOCATION}/zsh/zimrc ${HOME}/.zimrc
+ln -sfn ${DOTFILES_LOCATION}/zsh ${ZSH_HOME}
