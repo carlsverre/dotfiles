d26cd37c549a27158b689565398c60f4299e470a
Carl Sverre <carl@f0a.org>
2023-02-08 00:13:23 -0800

    better editor, new layout

--- config/zsh/custom/1-export.zsh
+++ config/zsh/custom/1-export.zsh
@@ -1,4 +1,9 @@
 export LANG=en_US.UTF-8
 export WEBI_WELCOME=0
-export EDITOR="${EDITOR:-vim}"
 export PAGER="less -FXRS -x4"
+
+if [[ "${REMOTE_CONTAINERS}" = "true" ]]; then
+    export EDITOR="code"
+else
+    export EDITOR="${EDITOR:-vim}"
+fi
