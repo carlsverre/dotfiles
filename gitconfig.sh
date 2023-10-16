8511ea2adad555cbec56fa333ffb06ff74b887f7
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2023-10-16 18:12:38 +0000

    merge rather than overwrite

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,6 +1,20 @@
 # configure gitconfig via a series of git commands to merge with any existing config
 #
-git config --global include.path "${ROOTDIR}/config/gitconfig"
-git config --global --add include.path .gitconfig.local
+
+# read existing includes
+INCLUDES=$(git config --global --get-all include.path)
+
+# new includes to add
+NEW_INCLUDES="
+${ROOTDIR}/config/gitconfig
+.gitconfig.local
+"
+
+# merge and update
+for INCLUDE in ${NEW_INCLUDES}; do
+  if [[ ! "${INCLUDES}" =~ "${INCLUDE}" ]]; then
+    git config --global --add include.path "${INCLUDE}"
+  fi
+done
 
 log_info "[ok] setup gitconfig"
